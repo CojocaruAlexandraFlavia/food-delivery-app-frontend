1f67ef21e55c7661f62932f0d0213bfdb2c1ebb7
Alexandra Cojocaru <alexandraflavia00@gmal.com>
2023-01-24 18:52:28 +0200

    add review component

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ import Register from './components/Register';
 import AddRestaurantManager from './components/AddRestaurantManager';
 import AddRestaurant from './components/AddRestaurant';
 import UpdateRestaurant from './components/UpdateRestaurant';
+import AddReview from './components/AddReview';
 
 function App() {
 
@@ -42,6 +43,7 @@ function App() {
                     <Route path='/add-restaurant-manager' element={<AddRestaurantManager/>}/>
                     <Route path='/add-restaurant' element={<AddRestaurant/>}/>
                     <Route path='/update-restaurant/:id' element={<UpdateRestaurant/>}/>
+                    <Route path='/add-review' element={<AddReview/>}/>
                 </Routes>
             </BrowserRouter>
         </UserContext.Provider>
