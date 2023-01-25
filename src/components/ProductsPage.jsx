fd5d99ad7509076408fed7e8df54aedf07ba6723
Andreea Gavrila <andreeagavrila02@gmail.com>
2023-01-25 21:27:38 +0200

    Product Page + Popup

--- src/components/ProductsPage.jsx
+++ src/components/ProductsPage.jsx
@@ -2,9 +2,61 @@ import React, { useEffect, useState } from 'react';
 import { Button, ButtonGroup, Container, Table } from 'reactstrap';
 import { Link } from 'react-router-dom';
 import { useParams } from "react-router"
+import Col from 'react-bootstrap/Col';
+import Modal from 'react-bootstrap/Modal';
+import Row from 'react-bootstrap/Row';
+
+
+function MydModalWithGrid(props) {
+  return (
+    <Modal {...props} 
+    size="lg"
+    aria-labelledby="contained-modal-title-vcenter"
+    centered
+    >
+      <Modal.Header closeButton>
+        <Modal.Title id="contained-modal-title-vcenter">
+          Using Grid in Modal
+        </Modal.Title>
+      </Modal.Header>
+
+      <Modal.Body className="show-grid">
+        <Container>
+          <Row>
+            <Col xs={12} md={8}>
+              .col-xs-12 .col-md-8
+            </Col>
+            <Col xs={6} md={4}>
+              .col-xs-6 .col-md-4
+            </Col>
+          </Row>
+
+          <Row>
+            <Col xs={6} md={4}>
+              .col-xs-6 .col-md-4
+            </Col>
+            <Col xs={6} md={4}>
+              .col-xs-6 .col-md-4
+            </Col>
+            <Col xs={6} md={4}>
+              .col-xs-6 .col-md-4
+            </Col>
+          </Row>
+        </Container>
+      </Modal.Body>
+
+      <Modal.Footer>
+        <Button onClick={props.onHide}>Close</Button>
+      </Modal.Footer>
+    </Modal>
+  );
+}
 
 
 const ProductsPage = () => {
+
+  const [modalShow, setModalShow] = useState(false);
+
 
     const {id} = useParams()
     const [products, setProducts] = useState({
@@ -16,6 +68,7 @@ const ProductsPage = () => {
         categoryName:"",
         restaurantName:""
     })
+    
     const [loading, setLoading] = useState(true);
   
     useEffect(() => {
@@ -35,6 +88,7 @@ const ProductsPage = () => {
 
     const productList = products.map(product => {
         const details = `${product.ingredients || ''} ${product.price || ''} ${product.discount || ''} ${product.availability || ''} ${product.categoryName || ''} ${product.restaurantName || ''}`;
+        
         return <tr key={product.id}>
           <td style={{whiteSpace: 'nowrap'}}>{product.name}</td>
           <td>{details}</td>
@@ -48,6 +102,15 @@ const ProductsPage = () => {
     
       return (
         <div>
+          <Button variant="primary" onClick={() => setModalShow(true)}>
+            Launch modal with grid
+          </Button>
+          
+          <MydModalWithGrid 
+          show={modalShow} 
+          onHide={() => setModalShow(false)} />
+
+
           <Container fluid>
             <div className="float-end">
               <Button color="success" tag={Link} to="/product/save">Add Product</Button>
@@ -68,6 +131,6 @@ const ProductsPage = () => {
           </Container>
         </div>
       );
-    };
+     };
 
 export default ProductsPage;
