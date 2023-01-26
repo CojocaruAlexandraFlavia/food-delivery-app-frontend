7b917721158e460fe04ed82fca70f3e07f52dfb6
Andreea Gavrila <andreeagavrila02@gmail.com>
2023-01-26 19:11:28 +0200

    Manage Products Page

--- src/components/ProductsPage.jsx
+++ src/components/ProductsPage.jsx
@@ -1,136 +1,77 @@
-import React, { useEffect, useState } from 'react';
-import { Button, ButtonGroup, Container, Table } from 'reactstrap';
-import { Link } from 'react-router-dom';
-import { useParams } from "react-router"
-import Col from 'react-bootstrap/Col';
-import Modal from 'react-bootstrap/Modal';
-import Row from 'react-bootstrap/Row';
-
-
-function MydModalWithGrid(props) {
-  return (
-    <Modal {...props} 
-    size="lg"
-    aria-labelledby="contained-modal-title-vcenter"
-    centered
-    >
-      <Modal.Header closeButton>
-        <Modal.Title id="contained-modal-title-vcenter">
-          Using Grid in Modal
-        </Modal.Title>
-      </Modal.Header>
-
-      <Modal.Body className="show-grid">
-        <Container>
-          <Row>
-            <Col xs={12} md={8}>
-              .col-xs-12 .col-md-8
-            </Col>
-            <Col xs={6} md={4}>
-              .col-xs-6 .col-md-4
-            </Col>
-          </Row>
-
-          <Row>
-            <Col xs={6} md={4}>
-              .col-xs-6 .col-md-4
-            </Col>
-            <Col xs={6} md={4}>
-              .col-xs-6 .col-md-4
-            </Col>
-            <Col xs={6} md={4}>
-              .col-xs-6 .col-md-4
-            </Col>
-          </Row>
-        </Container>
-      </Modal.Body>
-
-      <Modal.Footer>
-        <Button onClick={props.onHide}>Close</Button>
-      </Modal.Footer>
-    </Modal>
-  );
-}
+import { Fragment, useState, useCallback, useEffect } from "react"
+import { Button, Container, Modal } from "react-bootstrap"
+import UpdateProduct from "./UpdateProduct"
 
 
 const ProductsPage = () => {
 
-  const [modalShow, setModalShow] = useState(false);
+    const [allProducts, setAllProducts] = useState([])
+    const [deleted, setDeleted] = useState(false)
+    const [editModal, setEditModal] = useState(false)
 
+    const getAllProducts = useCallback(() => {
+        fetch("/product/get-all")
+            .then(response => response.json())
+            .then(response => setAllProducts(response))
+    }, [])
 
-    const {id} = useParams()
-    const [products, setProducts] = useState({
-        name:"",
-        ingredients:"",
-        price:"",
-        discount:"",
-        availability:"",
-        categoryName:"",
-        restaurantName:""
-    })
-    
-    const [loading, setLoading] = useState(true);
-  
     useEffect(() => {
-      setLoading(true);
-  
-      fetch(`/product/get-by-id/${id}`)
-        .then(response => response.json())
-        .then(response => {
-          setProducts(response)
-          setLoading(false);
+        getAllProducts()
+    }, [getAllProducts])
+    
+    const boxStyle = {boxShadow:"1px 1px 4px 4px lightgrey", padding:"5px"}
+
+    const deleteProduct = (id) => { 
+
+        fetch( `/product/delete-by-id/${id}`, {
+            method: "DELETE"
+        }).then(response => {
+            if(response.status === 200) {
+                getAllProducts()
+                setDeleted(true)
+                setTimeout(() => {
+                    setDeleted(false)
+                }, 5000)
+            }
         })
-    }, [id]);
-  
-    if (loading) {
-      return <p>Loading...</p>;
     }
 
-    const productList = products.map(product => {
-        const details = `${product.ingredients || ''} ${product.price || ''} ${product.discount || ''} ${product.availability || ''} ${product.categoryName || ''} ${product.restaurantName || ''}`;
-        
-        return <tr key={product.id}>
-          <td style={{whiteSpace: 'nowrap'}}>{product.name}</td>
-          <td>{details}</td>
-          <td>
-            <ButtonGroup>
-              <Button size="sm" color="primary" tag={Link} to={"/product/update/" + product.id}>Edit</Button>
-            </ButtonGroup>
-          </td>
-        </tr>
-      });
-    
-      return (
-        <div>
-          <Button variant="primary" onClick={() => setModalShow(true)}>
-            Launch modal with grid
-          </Button>
+    const closeModal = () => {
+        setEditModal(false)
+    }
+
+    return(
+        <Container>
+            {
+                allProducts.map((product, i) => <Fragment key={i}>
+                    <div style={boxStyle}>
+                        <h3>Name: {product.name}</h3>
+                        <h3>Price: {product.price}</h3>
+                        <h3>Discount: {product.discount}</h3>
+                        <h3>Ingredients: {product.ingredients}</h3>
+                        <h3>Quantity: {product.quantity}</h3>
+                        <h3>Availability: {product.availability}</h3>
+                        
+                        <br/>
+                        {
+                            deleted? <h3>Deleted successfully</h3>: null
+                        }
+
+                        <Button variant="danger" onClick={() => deleteProduct(product.id)}>Delete</Button>
+                        <Button onClick={() => setEditModal(true)}>Edit</Button>
+                    </div> <br/>
+
+                    <Modal show={editModal} onHide={closeModal}>
+                        <Modal.Header closeButton>Edit product</Modal.Header>
+                        <Modal.Body>
+                            <UpdateProduct productId={product.id}/>
+                        </Modal.Body>
+                    </Modal>
+                </Fragment>)
+            }
           
-          <MydModalWithGrid 
-          show={modalShow} 
-          onHide={() => setModalShow(false)} />
+        </Container>
+    )
+}
 
-
-          <Container fluid>
-            <div className="float-end">
-              <Button color="success" tag={Link} to="/product/save">Add Product</Button>
-            </div>
-            <h3>Products Page</h3>
-            <Table className="mt-4">
-              <thead>
-              <tr>
-                <th width="20%">Name</th>
-                <th width="20%">Details</th>
-                <th width="10%">Actions</th>
-              </tr>
-              </thead>
-              <tbody>
-              {productList}
-              </tbody>
-            </Table>
-          </Container>
-        </div>
-      );
-     };
-
-export default ProductsPage;
+export default ProductsPage
---
+++ src/components/UpdateProduct.jsx
@@ -0,0 +1,102 @@
+import { useState, useEffect } from "react"
+import { Button, Container, Form } from "react-bootstrap"
+import { useParams } from "react-router"
+
+const UpdateProduct = ({productId}) => {
+  
+  const {paramId} = useParams()
+  const id = paramId !== null && paramId !== undefined? paramId: productId
+  const [product, setProduct] = useState({
+      name:"",
+      price:"",
+      discount:"",
+      ingredients:""
+  })
+  const [errors, setErrors] = useState({})
+  const [updated, setUpdated] = useState(false)
+
+  useEffect(() => {
+    fetch(`/product/get-by-id/${id}`).then(response => response.json()).then(response => {
+      setProduct({
+        name: response.name,
+        price: response.price,
+        discount: response.discount,
+        ingredients: response.ingredients
+      })
+    })
+  }, [id])
+
+  const findFormErrors = () => {
+    const {name, price, discount, ingredients} = product
+    const newErrors = {}
+
+    if(name === "") newErrors.name = "Required field"
+    if(price === "") newErrors.price = "Required field"
+    if(discount === "") newErrors.discount = "Required field"
+    if(ingredients === "") newErrors.ingredients = "Required field"
+
+    return newErrors
+  }
+
+
+  const update = () => {
+    const newErrors = findFormErrors()
+    if(Object.keys(newErrors).length > 0) {
+        setErrors(newErrors)
+    } else {
+        fetch(`/product/update/${id}`, {
+            method: "PUT",
+            body: JSON.stringify(product),
+            headers: {
+                "Content-Type":"application/json"
+            }
+        }).then(response => {
+            if(response.status === 200) {
+                setUpdated(true)
+                setErrors({})
+                setTimeout(() => {
+                    setUpdated(false)
+                }, 3000)
+            } 
+        })
+    }
+}
+
+return(
+  <Container>
+      <Form>
+          <Form.Group>
+              <Form.Label>Name</Form.Label>
+              <Form.Control value={product.name} isInvalid={errors.name} onChange={(e) => setProduct({...product, name:e.target.value})}/>
+              <Form.Control.Feedback type="invalid">{errors.name}</Form.Control.Feedback>
+          </Form.Group>
+
+          <Form.Group>
+              <Form.Label>Price</Form.Label>
+              <Form.Control value={product.price} isInvalid={errors.price} onChange={(e) => setProduct({...product, price:e.target.value})}/>
+              <Form.Control.Feedback type="invalid">{errors.price}</Form.Control.Feedback>
+          </Form.Group>
+
+          <Form.Group>
+              <Form.Label>Discount</Form.Label>
+              <Form.Control value={product.discount} isInvalid={errors.discount} onChange={(e) => setProduct({...product, discount:e.target.value})}/>
+              <Form.Control.Feedback type="invalid">{errors.discount}</Form.Control.Feedback>
+          </Form.Group>
+
+          <Form.Group>
+              <Form.Label>Ingredients</Form.Label>
+              <Form.Control value={product.ingredients} isInvalid={errors.ingredients} onChange={(e) => setProduct({...product, ingredients:e.target.value})}/>
+              <Form.Control.Feedback type="invalid">{errors.ingredients}</Form.Control.Feedback>
+          </Form.Group>
+
+          <Button variant="success" onClick={update}>Update details</Button>
+          {
+              updated? <h3 style={{color:"green"}}>Details updated successfully!</h3>: null
+          }
+      </Form>
+  </Container>
+)
+
+}
+
+export default UpdateProduct
