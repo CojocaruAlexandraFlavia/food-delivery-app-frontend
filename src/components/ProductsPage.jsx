3d8c94b872bf88784d04bf63910e7b6e915b94a4
Andreea Gavrila <andreeagavrila02@gmail.com>
2023-01-27 14:02:52 +0200

    Save Product

--- src/components/ProductsPage.jsx
+++ src/components/ProductsPage.jsx
@@ -1,5 +1,5 @@
 import { Fragment, useState, useCallback, useEffect } from "react"
-import { Button, Container, Modal } from "react-bootstrap"
+import { Button, Container, Modal, Form } from "react-bootstrap"
 import { useParams } from "react-router"
 import UpdateProduct from "./UpdateProduct"
 
@@ -13,6 +13,19 @@ const ProductsPage = ({restaurantId}) => {
     const [setChanged] = useState(false)
     const [editModal, setEditModal] = useState(false)
     const [productId, setProductId] = useState(0)
+    
+    const [errors, setErrors] = useState({})
+    const [saveModal, setSaveModal] = useState(false)
+    const [productAdded, setProductAdded] = useState(false)
+    const [product, setProduct] = useState({
+      name:"",
+      price:"",
+      discount:"",
+      ingredients:"",
+      availability:true,
+      restaurant_id:restId,
+      category_id:""
+    })
 
     const getAllProducts = useCallback(() => {
         fetch(`/product/get-all-by-restaurantId/${restId}`)
@@ -63,6 +76,79 @@ const ProductsPage = ({restaurantId}) => {
       setEditModal(true)
     }
 
+    const closeSaveModal = () => {
+      setSaveModal(false)
+    }
+
+    const saveOpenModal = () => {
+      setSaveModal(true)
+    }
+
+
+    const findFormErrors = () => {
+      const {name, price, discount, ingredients, category_id} = product
+      const newErrors = {}
+      const requiredField = "Required field"
+
+      if(name === "") newErrors.name = requiredField
+      if(price === "") newErrors.price = requiredField
+      if(discount === "") newErrors.discount = requiredField
+      if(ingredients === "") newErrors.ingredients = requiredField
+      if(category_id === "") newErrors.category_id = requiredField
+      return newErrors
+    }
+
+    const onChange = (e) => {
+      const {id, value} = e.target
+      setProduct({ ...product, [id]: value });
+      if(errors[id]) {
+          setErrors({...errors, [id]: null})
+      }
+    }
+
+    const saveProduct = () => {
+
+      const newErrors = findFormErrors()
+        if(Object.keys(newErrors).length > 0) {
+            setErrors(newErrors)
+        } 
+        else 
+        {
+          console.log(product)
+
+          const controller = new AbortController()
+          const signal = controller.signal
+
+          fetch("/product/save", {
+              method:"POST",
+              body: JSON.stringify(product),
+              signal:signal,
+              headers:{
+                  "Content-Type":"application/json",
+                  "Accept":"application/json"
+              }
+          }).then(response => {
+              if(response.status === 200) {
+                setProductAdded(true)
+                  setErrors({})
+                  setProduct({
+                    name:"",
+                    price:"",
+                    discount:"",
+                    ingredients:"",
+                    availability:true,
+                    restaurant_id:restId,
+                    category_id:0
+                  })
+                  setTimeout(() => {
+                    setProductAdded(false)
+                  }, 5000)
+              }
+          })
+        }
+    }
+
+
     return(
         <Container>
             {
@@ -85,14 +171,54 @@ const ProductsPage = ({restaurantId}) => {
                   </div> <br/>
 
                   <Modal show={editModal} onHide={closeModal}>
-                      <Modal.Header closeButton>Edit product</Modal.Header>
-                      <Modal.Body>
-                          <UpdateProduct productId={productId}/>
-                      </Modal.Body>
+                    <Modal.Header closeButton>Edit product</Modal.Header>
+                    <Modal.Body>
+                        <UpdateProduct productId={productId}/>
+                    </Modal.Body>
                   </Modal>
               </Fragment>)
-            }
-          
+            }      
+
+            <Button variant="success" onClick={saveOpenModal}>Add product</Button>
+            <Modal show={saveModal} onHide={closeSaveModal}>
+              <Modal.Header closeButton>Add product</Modal.Header>
+              <Modal.Body>
+                <Form>
+                  <Form.Group>
+                      <Form.Label>Name</Form.Label>
+                      <Form.Control id={"name"} value={product.name} isInvalid={errors.name} onChange={onChange}/>
+                      <Form.Control.Feedback type={"invalid"}>{errors.name}</Form.Control.Feedback>
+                  </Form.Group>
+                  <Form.Group>
+                      <Form.Label>Price</Form.Label>
+                      <Form.Control id={"price"} value={product.price} isInvalid={errors.price} onChange={onChange}/>
+                      <Form.Control.Feedback type={"invalid"}>{errors.price}</Form.Control.Feedback>
+                  </Form.Group>
+                  <Form.Group>
+                      <Form.Label>Discount</Form.Label>
+                      <Form.Control id={"discount"} value={product.discount} isInvalid={errors.discount} onChange={onChange}/>
+                      <Form.Control.Feedback type={"invalid"}>{errors.discount}</Form.Control.Feedback>
+                  </Form.Group>
+                  <Form.Group>
+                      <Form.Label>Ingredients</Form.Label>
+                      <Form.Control id={"ingredients"} value={product.ingredients} isInvalid={errors.ingredients} onChange={onChange}/>
+                      <Form.Control.Feedback type={"invalid"}>{errors.ingredients}</Form.Control.Feedback>
+                  </Form.Group>
+                  <Form.Group>
+                      <Form.Label>Category_id</Form.Label>
+                      <Form.Control id={"category_id"} value={product.category_id} isInvalid={errors.category_id} onChange={onChange}/>
+                      <Form.Control.Feedback type={"invalid"}>{errors.category_id}</Form.Control.Feedback>
+                  </Form.Group>
+                <Button variant="success" onClick={saveProduct}>Add product</Button>
+                <br/><br/>
+                {
+                    productAdded? <h3 style={{color:"green"}}>Product added successfully</h3> : null
+                }
+                </Form>
+
+              </Modal.Body>
+            </Modal>
+
         </Container>
     )
 }
