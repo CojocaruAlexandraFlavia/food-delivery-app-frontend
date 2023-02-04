8898439d9791380c6c890cf20cf98aa1cb481005
Florea Maria <floreamaria262@gmail.com>
2023-02-04 15:07:40 +0200

    added all buttons for view cart

--- src/components/CartProducts.jsx
+++ src/components/CartProducts.jsx
@@ -1,13 +1,14 @@
 import { Fragment, useContext, useEffect, useState } from "react"
-import { useParams } from "react-router"
 import 'rsuite/dist/rsuite.min.css';
 import { Panel } from 'rsuite';
-import { Button, Container, Modal, Form, Icon} from "react-bootstrap"
+import { Button} from "react-bootstrap"
 import UserContext from "../components/context/UserContext"
 
 const CartProducts = () => {
 
     const {user} = useContext(UserContext)
+    const[deliveryTax, setDeliveryTax] = useState(0.0)
+    const[orderValue, setOrderValue] = useState(0.0)
     
     const[products, setProducts] = useState([])
 
@@ -23,7 +24,40 @@ const CartProducts = () => {
                 "Content-Type": "application/json"
             }
         })
+        window.location.reload(false);
       }
+
+      const increaseProductQuantity=(id) =>{
+        const requestBody ={
+            "clientId":user.id,
+            "productId":id
+        }
+        fetch( `/order/cart/increase-quantity`, {
+            method: "POST",
+            body:JSON.stringify(requestBody),
+            headers: {
+                "Content-Type": "application/json"
+            }
+        })
+        window.location.reload(false);
+      }
+
+      const deleteProduct =(id) =>{
+        const requestBody ={
+            "clientId":user.id,
+            "productId":id
+        }
+        fetch( `/order/cart/delete-product`, {
+            method: "DELETE",
+            body:JSON.stringify(requestBody),
+            headers: {
+                "Content-Type": "application/json"
+            }
+        })
+        window.location.reload(false);
+      }
+     
+      
 
     useEffect(() => {
 
@@ -32,13 +66,14 @@ const CartProducts = () => {
       
             }).then(response => response.json()).then(response => {
                 setProducts(response.products)
+                setDeliveryTax(response.deliveryTax)
+                setOrderValue(response.value)
           
             })
         }
    
   }, [user.id])
     return(
-
         <Fragment>
             {
                 products !== undefined ? products.map((product, i) => <div key={i}>
@@ -51,34 +86,66 @@ const CartProducts = () => {
                     <h3> {product.quantity}x {product.productDto.name}</h3>
 
                     <table >
-                    <tr>initial price: 
+                    <br></br>
+                    <tr>Initial price: 
                         <td style={{paddingLeft:10}}>{ product.productDto.price * product.quantity}  lei</td>
                     </tr>
-                    <tr>  discount:
+                    <tr>Discount:
                         <td style ={{color:"#d41919"}}>{product.productDto.discount}%</td>
                     </tr>
-                    <tr>price: 
+                    <tr>Price: 
                         <td>{(product.productDto.price -
                         (product.productDto.discount / 100 * product.productDto.price)) * product.quantity} lei</td>
                     </tr>
                     </table>
 
                     <br></br>
+                    <br></br><br>
+
+                    </br>
                     <Button variant="secondary" onClick={() => decreaseProductQuantity(product.productDto.id)} negative className='quan-buttons'  style={{width:40, height:40, alignContent:"center"}}color="blue"> - </Button>
                 {
                 <input type="text" id="quantity"name="quantity" value={product.quantity} style={{width:40, alignContent:"center", paddingBottom:6}} />
                 }
-                <Button className='quan-buttons' style={{width:40, height:40, alignContent:"center"}} onClick="">
+                <Button className='quan-buttons' onClick={() => increaseProductQuantity(product.productDto.id)} style={{width:40, height:40, alignContent:"center"}} >
                  + </Button>
-                <br></br>
+                <Button className="delete-button"  onClick={() => deleteProduct(product.productDto.id)} style={{width:90, height:40, alignContent:"center", backgroundColor:"red", borderColor:"red", float:"right"}} >Remove</Button>
                 </Panel>
                 </div>
-                <div className='quanity-group'>
-              
-                </div>
-                  
+                
             </div>): null
+            
             }
+            <br></br>
+          
+            <div style={{
+                    display: 'block', width: 800, paddingLeft: 140
+                }}>
+                    <Panel shaded fontSize="30px">
+                   
+                    <table >
+               
+                    <tr> <h3>Total:</h3>
+                        <td> 
+                        <h3> {deliveryTax + orderValue}</h3>
+                        </td>    
+                    </tr>
+                    <tr>Delivery tax: 
+                        <td style={{paddingLeft:10}}>{deliveryTax}  lei</td>
+                    </tr>
+                    <tr> Products price:
+                        <td style ={{paddingLeft:10}}> {orderValue.toPrecision(4)} lei</td>
+                    </tr>
+                    </table>
+                    <Button shaded className="delete-button" style={{width:120, height:40, alignContent:"center", backgroundColor:"#73BA9B", borderColor:"#73BA9B", float:"right", marginBottom:10}} >SEND</Button>
+              
+                    <br></br>
+                    </Panel>
+                </div>
+                <br></br>
+                <br></br>
+            
+          
         </Fragment>
     )
 
